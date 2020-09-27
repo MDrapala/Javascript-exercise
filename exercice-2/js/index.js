0c3135ef2dbbcc27d4cc2137dc6e2f00005c03f5
MDrapala <drapalamathieu95@gmail.com>
2020-09-27 13:47:52 +0200

    feat(drawBar): creation of a 100% responsive loading bar

--- exercice-2/js/index.js
+++ exercice-2/js/index.js
@@ -1,22 +1,35 @@
 'use strict';
+
 class Drawbar {
 	constructor(sum, nbr) {
 		this.sum = sum;
 		this.nbr = nbr;
 	}
+
+	/*
+	 * Initalize All elements
+	 */
 	get createElements() {
 		document.body.style.width = '90%';
 		document.body.style.margin = '30px auto 0';
 		this.divBar(this.divSkill());
 	}
-	//Create Elements for first div
+
+	/*
+	 * Initalize body with divBar
+	 * @return insert in the body divSkill
+	 */
 	divSkill() {
 		var divSkill = document.createElement('div');
 		divSkill.id = 'skill';
 		this.styleSkill(divSkill);
 		return document.body.appendChild(divSkill);
 	}
-	//Add style into first Div
+
+	/*
+	 * Initalize style CSS for the proportion of the background segment
+	 * @param {feature} insert the creation of the background bar and responsive
+	 */
 	styleSkill(divSkill) {
 		divSkill.style.overflow = 'hidden';
 		divSkill.style.height = '18px';
@@ -28,19 +41,36 @@ class Drawbar {
 		divSkill.style.gridTemplateColumns = 'repeat(auto-fit, minmax 250px, 1fr))';
 		divSkill.style.gridAutoRows = '150px';
 	}
-	//Create Elements for seconde div
+
+	/*
+	 * Initalize divSkill with divBar
+	 * @param {feature} insert the creation of the background drawBar
+	 * @return insert in the divBar into divSkill
+	 */
 	divBar(divSkill) {
 		var divBar = document.createElement('div');
 		divBar.id = 'bar';
 		this.styleBar(divBar);
 		return divSkill.appendChild(divBar);
 	}
+
+	/*
+	 * Initalize style CSS for the proportion of the loaded segment
+	 * @param {feature} insert the creation of the loading bar
+	 */
 	styleBar(divBar) {
+		console.log(divBar);
 		divBar.style.width = this.nbr + '%';
 		divBar.style.height = '18px';
 		divBar.style.backgroundColor = '#0e90d2';
 		divBar.style.transition = 'width 0.6s ease';
 	}
 }
+
+/*
+ * Initalize size draw bar
+ * @param {number} the total sum of the number
+ * @param{number} the proportion of the loaded segment of the bar
+ */
 const bar = new Drawbar(1000, 30);
 console.log(bar.createElements);
