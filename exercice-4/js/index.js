4a907c165f8a823e5df2a35a1616a168a4105ac0
MDrapala <drapalamathieu95@gmail.com>
2020-10-13 10:43:47 +0200

    feat(renderWorldMap): Creation of an interactive map

--- exercice-4/js/index.js
+++ exercice-4/js/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function renderWorldMap(pays) {
+    var createSpan = document.createElement('span');
+    createSpan.textContent = pays + "\n / ";
+    document.body.appendChild(createSpan);
+    document.getElementById(pays).style.fill = "red";
+
+}
