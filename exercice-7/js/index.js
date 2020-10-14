41bef0f5e42fc5a4cd5dd571fb56c83ecdcf2e02
MDrapala <drapalamathieu95@gmail.com>
2020-10-14 15:50:34 +0200

    feat(average): Calculates the average of an array

--- exercice-7/js/index.js
+++ exercice-7/js/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let notes = [10, 13, 13, 12, 15, 12, 11, 16, 14]
+compteNotes(notes);
+
+function compteNotes(notes){
+    let count = 0;
+
+    for(let i = 0; i < notes.length; i++){
+        count += parseFloat(notes[i], 20);
+    }
+
+    let result = count/notes.length
+    document.write("the sum of all notes is:" + result);
+}
